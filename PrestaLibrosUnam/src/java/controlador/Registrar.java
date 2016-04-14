b5449ba3a69975220ffc7de1aac3d08b32b10e6a
jonathanjb <jonathanjb@debian.LocalHost.net>
2016-04-14 16:07:11 -0500

    parte del registro de usuario modificando el UsuarioDAO para obtener el maximo indice de la tabla usuario

--- PrestaLibrosUnam/src/java/controlador/Registrar.java
+++ PrestaLibrosUnam/src/java/controlador/Registrar.java
@@ -4,6 +4,7 @@ import java.util.Date;
 import java.util.Iterator;
 import java.util.List;
 import modelo.Usuario;
+import modelo.UsuarioDAO;
 import javax.faces.bean.ManagedBean;
 import org.hibernate.HibernateException;
 import org.hibernate.SQLQuery;
@@ -19,99 +20,131 @@ import org.hibernate.cfg.Configuration;
  */
 public class Registrar {
 
-    //public Usuario usuario;
+    public Usuario usuario;
   
     private String nombe;//Nombre de el usuario.
     private String app;//Apellido paterno del usuario.
     private String apm;//Apellido materno del usuario.
     private String password;//contrasenia del usuario.
-    private Date fechaNacimiento;//fecha del usuario.
     private int telefono;//Teleffono del usuario.
     private String facultad;//Facultad del usuario. 
-
+    private String correo;//Correo del usuario.
+    /**
+     * 
+     * @return  el correo del usuario
+     */
+    public String getCorreo() {
+        return correo;
+    }
+    /**
+     * 
+     * @param correo del usuario
+     */
+    public void setCorreo(String correo) {
+        this.correo = correo;
+    }
+    /**
+     * 
+     * @return el nombre el del usuario 
+     */
     public String getNombe() {
         return nombe;
     }
-
-    public void setNombe(String nombe) {
-        this.nombe = nombe;
+    /**
+     * 
+     * @param nombre el nombre el del usuario 
+     */
+    public void setNombe(String nombre) {
+        this.nombe = nombre;
     }
-
+    /**
+     * 
+     * @return El Apellido paterno del usuario
+     */
     public String getApp() {
         return app;
     }
-
+    /**
+     * 
+     * @param app. El Apellido paterno del usuario
+     */
     public void setApp(String app) {
         this.app = app;
     }
-
+    /**
+     * 
+     * @return El apellido materno del usuario
+     */
     public String getApm() {
         return apm;
     }
-
+    /**
+     * 
+     * @param apm. El apellido paterno de el usuario
+     */
     public void setApm(String apm) {
         this.apm = apm;
     }
-
+    /**
+     * 
+     * @return La contrasenia del usuario 
+     */
     public String getPassword() {
         return password;
     }
-
+    /**
+     * 
+     * @param password La contrasenia del usuario
+     */
     public void setPassword(String password) {
         this.password = password;
     }
-
-    public Date getFechaNacimiento() {
-        return fechaNacimiento;
-    }
-
-    public void setFechaNacimiento(Date fechaNacimiento) {
-        this.fechaNacimiento = fechaNacimiento;
-    }
-
+    /**
+     * 
+     * @return el telefono del usuario 
+     */
     public int getTelefono() {
         return telefono;
     }
-
+    /**
+     * 
+     * @param telefono el telefono de el usuario. 
+     */
     public void setTelefono(int telefono) {
         this.telefono = telefono;
     }
-
+    /**
+     * 
+     * @return La facultad de el usuario
+     */
     public String getFacultad() {
         return facultad;
     }
-
+    /**
+     * 
+     * @param facultad la Facultad de el usuario. 
+     */
     public void setFacultad(String facultad) {
         this.facultad = facultad;
     }
     
   public void Registrar() {
-      
+        usuario = new Usuario();
+        UsuarioDAO user = new UsuarioDAO();
+        try{
+             usuario.setNombre(this.getNombe());
+             usuario.setApp(this.getApp());
+             usuario.setApm(this.getApm());
+             usuario.setContrasenia(this.getPassword());
+             usuario.setFacultad(this.getFacultad());
+             usuario.setFacultad(this.getFacultad());
+             usuario.setCorreo(this.getCorreo());
+             //usuario.setFotoUsr();
+             usuario.setIdusuario(user.maxIndice());
+             user.save(usuario);
+        }catch(Exception e){
+        }
   }
   
-  private int maxIndice(){
-      SessionFactory factory;
-        try {
-            factory = new Configuration().configure().buildSessionFactory();
-        } catch (Throwable ex) {
-            System.err.println("Failed to create sessionFactory object." + ex);
-            throw new ExceptionInInitializerError(ex);
-        }
-        int max=-1;
-        Session session = factory.openSession();
-        Transaction tx = null;
-        try {
-            tx = session.beginTransaction();
-            String sql = "SELECT max(idusuario) FROM usuarios";
-            SQLQuery query = session.createSQLQuery(sql);
-            max = (int)query.uniqueResult();
-            tx.commit();
-            //return max;
-        }catch(HibernateException e){
-            
-        }finally{
-            session.close();
-        }
-      return max;
-  }
+  
 }
--- PrestaLibrosUnam/src/java/modelo/UsuarioDAO.java
+++ PrestaLibrosUnam/src/java/modelo/UsuarioDAO.java
@@ -1,6 +1,12 @@
 package modelo;
 
 import java.util.List;
+import org.hibernate.HibernateException;
+import org.hibernate.SQLQuery;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
+import org.hibernate.cfg.Configuration;
 
 public class UsuarioDAO extends AbstractDAO {
 
@@ -53,5 +59,35 @@ public class UsuarioDAO extends AbstractDAO {
     public List findAll() throws DataAccessLayerException {
         return super.findAll(Usuario.class);
     }
+    
+    /**
+     * Metedo que obtinen el maximo indice de la tabla usuario.
+     * @return max, el Maximo indice de la tabla usuario. 
+     */
+    public int maxIndice(){
+      SessionFactory factory;
+        try {
+            factory = new Configuration().configure().buildSessionFactory();
+        } catch (Throwable ex) {
+            System.err.println("Failed to create sessionFactory object." + ex);
+            throw new ExceptionInInitializerError(ex);
+        }
+        int max=-1;
+        Session session = factory.openSession();
+        Transaction tx = null;
+        try {
+            tx = session.beginTransaction();
+            String sql = "SELECT max(idusuario) FROM usuarios";
+            SQLQuery query = session.createSQLQuery(sql);
+            max = (int)query.uniqueResult();
+            tx.commit();
+            //return max;
+        }catch(HibernateException e){
+            
+        }finally{
+            session.close();
+        }
+      return max;
+  }
     
 }
