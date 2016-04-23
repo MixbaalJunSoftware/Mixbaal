5acb31827ec50dc2bc3b1d2f2270d1739cbe34fd
Dany <daniicape19@gmail.com>
2016-04-23 15:29:40 -0500

    Ver publicaciones!

--- PrestaLibrosUnam/src/java/controlador/verPublica.java
+++ PrestaLibrosUnam/src/java/controlador/verPublica.java
@@ -1,8 +1,29 @@
 package controlador;
 
-public class verPublica {
+import java.io.Serializable;
+import java.util.List;
+import javax.annotation.PostConstruct;
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.ViewScoped;
+import javax.faces.bean.ManagedProperty;
+import modelo.Libro;
+import modelo.LibroDAO;
 
-  public void ver() {
-  }
+@ManagedBean
+@ViewScoped
 
+public class verPublica implements Serializable {
+
+    private List<Libro> libros;
+       
+    @PostConstruct
+    public void ver() {
+        LibroDAO lib = new LibroDAO();
+        libros = lib.findAll();
+    }
+ 
+    public List<Libro> getLibros() {
+        return libros;
+    }
+ 
 }
